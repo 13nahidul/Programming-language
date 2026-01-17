b14c469e234c801f6b738c91b2b8fe0bd09f591c
Md Nahidul Islam <13nahidul@gmail.com>
2026-01-17 17:52:36 +0600

    Add files via upload

---
+++ sum_digits_of_a_number.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+#include <stdio.h>
+
+int sumdigit(int m)
+{
+    int c, s=0;
+
+    while (m>0)
+    {
+        c = m%10;   // got last digit
+        s = s+c;   // add digit
+        m = m/10;  // removed last digit
+    }
+    return s;
+}
+
+int main()
+{
+    int number;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    printf("Summation is = %d", sumdigit(number));
+    return 0;
+}
+
